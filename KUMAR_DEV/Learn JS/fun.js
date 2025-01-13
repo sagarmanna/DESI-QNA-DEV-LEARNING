61aead28c11cbe3b7cd954031daeb717965cd0f8
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-01-13 21:53:37 +0530

    Add files via upload

---
+++ KUMAR_DEV/Learn JS/fun.js
@@ -0,0 +1,32 @@
+console.log("This is about function");
+
+
+function mySubjects(subject,message){
+    console.log(subject+message);
+}
+
+let subj="CN";
+let subj2="DBMS";
+let subj3="SE";
+let subj4="Phython";
+let subj5="Web Dev";
+let subj6="COE";
+let msg=": This is your subject";
+
+
+
+
+// console.log(subj +"  " +"This is your subject");
+// console.log(subj2 +"  " +"This is your subject");
+// console.log(subj3 +"  " +"This is your subject");
+// console.log(subj4 +"  " +"This is your subject");
+// console.log(subj5 +"  " +"This is your subject");
+
+mySubjects(subj,msg);
+mySubjects(subj2,msg);
+mySubjects(subj3,msg);
+mySubjects(subj4,msg);
+mySubjects(subj5,msg);
+
+
+//Returning a value
---
+++ KUMAR_DEV/Learn JS/function.js
@@ -0,0 +1,25 @@
+console.log("This is about function");
+
+
+function mySubjects(subjects){
+
+   console.log(subjects+": This is your subjects "); 
+}
+let subject1 ="CN";
+let subject2="DBMS";
+let subject3="SE";
+let subject4="PY";
+let subject5="Web DEV";
+
+
+mySubjects(subject1);
+mySubjects(subject2);
+mySubjects(subject3);
+mySubjects(subject4);
+mySubjects(subject5);
+
+// console.log(subject1+": This is your subjects");
+// console.log(subject2+": This is your subjects");
+// console.log(subject3+": This is your subjects");
+// console.log(subject4+": This is your subjects");
+// console.log(subject5+": This is your subjects");
